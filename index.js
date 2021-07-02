0febff31fcb2f0644185408332b51fad348c21e9
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-07-02 17:21:12 +0200

    Comprobar si la linea indicada existe y su mensaje

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+// imports
+
 require("dotenv").config();
 const fetch = require("node-fetch");
 const chalk = require("chalk");
 const { preguntasUsuario } = require("./usuario");
 
+// Declaraciones API
+
 const urlLineaMetro = process.env.URL_LINEA_METRO;
 const appKey = process.env.APP_KEY;
 const appId = process.env.APP_ID;
 
-const urlLiniasAPI = `${urlLineaMetro}app_id=${appId}&app_key=${appKey}`;
+const urlLineasAPI = `${urlLineaMetro}app_id=${appId}&app_key=${appKey}`;
 
-const cargarLinia = async () => {
-  const resp = await fetch(urlLiniasAPI);
-  const linias = await resp.json();
-  console.log(linias);
-};
+// He pasado la función dentro de init, porque se necesita arrayLineas dentro de init.
+//
+// const cargarLinia = async () => {
+//   const resp = await fetch(urlLineasAPI);
+//   const lineas = await resp.json();
+//   const arrayLineas = lineas.features.map(
+//     (linea) => linea.properties.NOM_LINIA
+//   );
+// };
 
-cargarLinia();
+// Función que recibe respuestas del usuario
 
 const init = async () => {
   const respuestas = await preguntasUsuario();
@@ -33,6 +41,26 @@ const init = async () => {
       break;
     default:
       break;
+  }
+  // cargarLinia();
+
+  // Fetch a la url
+
+  const resp = await fetch(urlLineasAPI);
+  const lineas = await resp.json();
+
+  // Comprobar si existe la linea
+
+  const arrayLineas = lineas.features.map(
+    (linea) => linea.properties.NOM_LINIA
+  );
+  if (!arrayLineas.includes(respuestas.linea.toUpperCase())) {
+    if (respuestas.informeErrores) {
+      console.log("La linea indicada no existe");
+      process.exit(1);
+    } else {
+      process.exit(1);
+    }
   }
 };
 
