59ac16cf482109131783d8fb43809b8bf265d06d
Pol Reig <polreigbroto@gmail.com>
2021-07-03 12:01:50 +0200

    Función guardar paradas

---
+++ guardarParadas.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const guardarParadas = (paradas) => {
+  fs.writeFile("infoParadas/paradas.txt", paradas, (err) => {
+    if (err) {
+      console.log(chalk.bold.red("¡Algo ha salido mal!"));
+    }
+    process.exit(1);
+  });
+};
+
+module.exports = {
+  guardarParadas,
+};
